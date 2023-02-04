1d09c5f0a17996b305730380c154578b7650f8f7
nidhisree123 <124237891+nidhisree123@users.noreply.github.com>
2023-02-04 13:52:58 +0530

    Add files via upload

---
+++ mth max and nth max.py
@@ -0,0 +1,10 @@
+n=int(input("enter the number: "))
+l=[]
+for i in range(n):
+    l.append(int(input("enter the elements: ")))
+a=int(input("enter the value of m: "))
+b=int(input("enter the value of n: "))
+l=sorted(l)
+print("mth maximum=",l[-a])
+print("nth minimum=",l[b-1])
+print("sum=",l[b-1]+l[-a],"sub=",l[-a]-(l[b-1]))
